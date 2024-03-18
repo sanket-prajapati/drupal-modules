16a6eafb021a34311535c998aed00d8a86377582
sanket-prajapati <sanketprajapati0209@gmail.com>
2024-03-18 17:35:43 +0000

    Added dependecy injection.

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -5,20 +5,51 @@ namespace Drupal\wpweb_infotech\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Url;
 use Drupal\file\Entity\File;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 
 class ListController extends ControllerBase{
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface;
+   */
+  protected $module_handler;
+
+  /**
+   * The database service.
+   * @param \Drupal\Core\Database\Connection $database
+   * 
+   * The module handler service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   */
+  public function __construct(Connection $database, ModuleHandlerInterface $module_handler){
+    $this->database = $database;
+    $this->module_handler =  $module_handler;
+  }
+
+  public static function create(ContainerInterface $container){
+    return new static (
+      $container->get('database'),
+      $container->get('module_handler')
+    );
+  }
 
   public function formDataList(){
 
-    // Something missing in query builder for date_formate expression
+    // Fetch details of records submitted
     // $result = \Drupal::database()->select('wpweb_infotech', 'wp')
     //   ->fields('wp',['name', 'gender', 'dob', 'profile_photo_fid'])
     //   // ->addExpression('DATE_FORMAT(wp.dob, "%Y-%m-%d")', 'dob')
     //   ->execute()
     //   ->fetchAll();
     
-    $result = \Drupal::database()->query("select name, gender, DATE_FORMAT(dob, '%Y-%m-%d') as dob, profile_photo_fid from wpweb_infotech")->fetchAll();
-
+    // $result = \Drupal::database()->query("select name, gender, DATE_FORMAT(dob, '%Y-%m-%d') as dob, profile_photo_fid from wpweb_infotech")->fetchAll();
+    $result = $this->database->query("select name, gender, DATE_FORMAT(dob, '%Y-%m-%d') as dob, profile_photo_fid from wpweb_infotech")->fetchAll();
     $header = [
       'name' => 'Name',
       'gender' => 'Gender',
@@ -32,11 +63,12 @@ class ListController extends ControllerBase{
       $profile_photo_fid = $record->profile_photo_fid;
       if($profile_photo_fid){
         $file = File::load($profile_photo_fid);
-        $file_uri = $file->uri->value;//or $file->getFileUri();//"public://profile_photo/IMG_20211127_174339_1-min.jpg"
-        $file_absolute_url = file_create_url($file_uri);//"http://localhost/web/d8-int-practice/web/sites/default/files/profile_photo/IMG_20211127_174339_1-min.jpg"
+        $file_uri = $file->uri->value;
+        $file_absolute_url = file_create_url($file_uri);
       }
       else{
-        $module_path = \Drupal::service('module_handler')->getModule('wpweb_infotech')->getPath();
+        // $module_path = \Drupal::service('module_handler')->getModule('wpweb_infotech')->getPath();
+        $module_path = $this->module_handler->getModule('wpweb_infotech')->getPath();
         $file_absolute_url = '../' . $module_path . '/image/profile.jpg';
       }
       $rows[] = [
@@ -47,10 +79,7 @@ class ListController extends ControllerBase{
       ];
     }
 
-    // $build['add_user'] = [
-    //   '#markup' => t("<button class='btn btn-primary'><a href='/wpweb-infotech/custom-form'>Add user</a></button>")
-    // ];
-
+    // Make links for add user
     $url_object = Url::fromRoute('wpweb_infotech.simple_form');
     $build['add_user'] = [
       '#type' => 'link',
--- src/Form/WpwebInfotechForm.php
+++ src/Form/WpwebInfotechForm.php
@@ -5,18 +5,61 @@ namespace Drupal\wpweb_infotech\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\file\Entity\File;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 class WpwebInfotechForm extends FormBase{
-  /**
-   * getFormId()
-   */
 
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * @var \Drupal\Core\Messenger\MessengerInterface;
+   */
+  protected $messenger;
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface;
+   */
+  protected $config;
+
+  /**
+   * The database service.
+   * @param \Drupal\Core\Database\Connection $database
+   * @param \Drupal\ $messenger
+   * */
+
+  public function __construct(Connection $database, MessengerInterface $messenger, ConfigFactoryInterface $config){
+    $this->database = $database;
+    $this->messenger =  $messenger;
+    $this->config = $config;
+  }
+
+  public static function create(ContainerInterface $container){
+    return new static (
+      $container->get('database'),
+      $container->get('messenger'),
+      $container->get('config.factory'),
+    );
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId(){
     return 'wpweb_infotech_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm($form, FormStateInterface $form_state){
-    $config = \Drupal::configFactory()->getEditable('wpweb_infotech.settings');
+    // $config = \Drupal::config('wpweb_infotech.settings');
+    $config = $this->config('wpweb_infotech.settings');
     if($config->get('wpweb_date')){
       $form['wpweb']['config_date'] = [
         '#type' =>'markup',
@@ -49,8 +92,6 @@ class WpwebInfotechForm extends FormBase{
     $form['wpweb']['profile_photo'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Profile photo'),
-      // '#upload_location' => 'private://profile', //Giving error bcz we not set this in setting.php
-      // '#upload_location' => 'public://sites/default/files/profile_photo', //It will set whole path inside files directory, resulting dublicating path
       '#upload_location' => 'public://profile_photo',
       '#upload_validators' => [
         'file_validate_extensions' => ['jpg jpeg gif png webp'],
@@ -66,22 +107,42 @@ class WpwebInfotechForm extends FormBase{
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function validateForm(&$form, FormStateInterface $form_state){
+    // Validate Name field.
+    $name_value = $form_state->getValue('name');
+    if (empty($name_value)) {
+      $form_state->setErrorByName('name', $this->t('Name field cannot be empty.'));
+    } elseif (!preg_match('/^[a-zA-Z\s]+$/', $name_value)) {
+      $form_state->setErrorByName('name', $this->t('Name field should not contain number and special characters.'));
+    }
 
+    // Validate DOB field based on configuration.
+    // $config = \Drupal::config('wpweb_infotech.settings');
+    $config = $this->config('wpweb_infotech.settings');
+    $wpweb_date = $config->get('wpweb_date');
+    $dob_value = $form_state->getValue('dob');
+    if (!empty($dob_value) && strtotime($dob_value) < strtotime($wpweb_date)) {
+      $form_state->setErrorByName('dob', $this->t('DOB must be on or after @date', ['@date' => $wpweb_date]));
+    }
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(&$form, FormStateInterface $form_state){
 
     $form_file = $form_state->getValue('profile_photo', 0);
-    // dump($form_file);//file id array
     if (isset($form_file[0]) && !empty($form_file[0])) {
       $file = File::load($form_file[0]);
-      // dump($file);//file with all file detials
       $file->setPermanent();
       $file->save();
     }
     
-    \Drupal::database()->insert('wpweb_infotech')
+    // \Drupal::database()->insert('wpweb_infotech')
+    $this->database->insert('wpweb_infotech')
     ->fields([
       'name' => $form_state->getValue('name'),
       'gender' => $form_state->getValue('gender'),
@@ -92,6 +153,7 @@ class WpwebInfotechForm extends FormBase{
     
     // Redirect to list page
     $form_state->setRedirect('wpweb_infotech.form_data_list');
-    \Drupal::messenger()->addMessage(t('@name Your form is submited successfully', ['@name' => $form_state->getValue('name')]));
+    // \Drupal::messenger()->addMessage(t('@name Your form is submited successfully', ['@name' => $form_state->getValue('name')]));
+    $this->messenger->addMessage(t('@name Your form is submited successfully', ['@name' => $form_state->getValue('name')]));
   }
 }
